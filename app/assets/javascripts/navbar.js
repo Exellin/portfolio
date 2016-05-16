7069ad4f431fbf2f1489273f1c4447e2e7008a8e
Exellin <carnegieshawn@yahoo.ca>
2016-05-16 21:59:57 +0000

    fixed z-index of navbar

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,20 +2,6 @@
 $(document).ready(function() {
   $('#nav-toggle').click(function() {
     $('.nav-container ul').slideToggle();
-    var links = $('.nav-container ul li').length;
-    var link_height = parseInt($('.nav-container ul li').css('height'), 10);
-    var fullPadding = link_height*links;
-    var currentPadding = $('.navigation').css('padding-bottom');
-    var marginChange = fullPadding - 70;
-    
     this.classList.toggle('active');
-    (currentPadding == '0px') ? (currentPadding = fullPadding) : (currentPadding = '0px');
-    $('.navigation').animate({paddingBottom: currentPadding});
-    
-    if (currentPadding == '0px') {
-      $('.navigation').animate({marginBottom: "+=" + marginChange});
-    } else if ((currentPadding == fullPadding)) {
-      $('.navigation').animate({marginBottom: "-=" + marginChange});
-   }
   });
 });
