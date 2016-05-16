b2a7993fae44ffa5e470321730128e76f474f54a
Exellin <carnegieshawn@yahoo.ca>
2016-05-16 23:07:26 +0000

    navbar transparent with no scroll

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,5 +1,16 @@
 /*global $*/
 $(document).ready(function() {
+  var scroll;
+
+  $(document).scroll(function() {
+    scroll = $(this).scrollTop();
+    if(scroll > 0) {
+      $('.navigation').css('background', '#262626');
+    } else {
+      $('.navigation').css('background', 'transparent');
+      }
+  });
+  
   $('#nav-toggle').click(function() {
     $('.link-container ul').slideToggle();
     this.classList.toggle('active');
