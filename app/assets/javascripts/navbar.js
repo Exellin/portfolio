f41f6305b24706bc636503c66a9e370bd0773338
Exellin <carnegieshawn@yahoo.ca>
2016-05-16 22:47:50 +0000

    keep navbar at top of page

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,7 +1,7 @@
 /*global $*/
 $(document).ready(function() {
   $('#nav-toggle').click(function() {
-    $('.nav-container ul').slideToggle();
+    $('.link-container ul').slideToggle();
     this.classList.toggle('active');
   });
 });
