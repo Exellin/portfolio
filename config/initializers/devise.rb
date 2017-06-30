aa3d02c5105a1d0d0d3c556ddf7a56750e64e8c5
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-06-29 22:45:30 -0600

    add figaro environment variables to secrets and database

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = ENV['DEVISE_SECRET_KEY'] ? Rails.env.production? : Rails.env.staging?
+  config.secret_key = Figaro.env.devise_secret_key ? Rails.env.production? : Rails.env.staging?
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
